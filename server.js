2b55b54469388f704170933522fc965a03b32625
ual-laptop <ianfielding@arizona.edu>
2023-04-13 16:04:46 -0700

    Minor adjustments

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const app = express();
 const port = 5000;
 app.use(express.json());
 const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'homepage')))
+app.use('/static', express.static(path.join(__dirname, 'homepage')));
+//app.use('/ray', express.static(path.join(__dirname, 'homepage/RayTracer')));
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/homepage/index.html");
+});
+
+app.get("/projects/raytracer",function(req,res){
+    res.sendFile(__dirname+"/homepage/RayTracer/raytraceIndex.html");
+});
+
+["Builder.js","Index.html","Color.js","MathUtils.js","Client.js","Style.css"].forEach(function(com){
+    app.get(`/projects/raytrace${com}`,function(req,res){
+        res.sendFile(__dirname+`/homepage/RayTracer/raytrace${com}`);
+    });
+})
+
+app.get('*', function(req, res){
+  res.status(404).send("What??");
 });
 
 app.listen(port, function() {

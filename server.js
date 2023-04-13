048a87a2da477251e3a076c3baa6f2ec961c55ca
ual-laptop <ianfielding@arizona.edu>
2023-04-13 10:31:29 -0700

    added homepage

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-//app.use(express.static(""));
 app.use(express.json());
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'homepage')))
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/homepage/index.html");
+});
 
 app.listen(port, function() {
     console.log(`App listening at http://localhost:${port}`);
